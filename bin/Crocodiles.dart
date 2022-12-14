bd2e40a97f6529239727eacd8718ecb02d0cfa49
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-14 18:55:44 +0200

    Update Crocodiles.dart

--- bin/Crocodiles.dart
+++ bin/Crocodiles.dart
@@ -1,21 +1,26 @@
-import 'dart:io';
+// import packages and classes
 
+import 'dart:io';
 import 'Animal_class.dart';
 
+// class called Amphibians extends from the class Animals
 class Amphibians extends Animals {
+  
+  //variables
   String? food;
   num long = 0;
 
+  // override Functions from Master Class
   @override
-  void info() {
+  void generalKnowledge() {
     print("enter the kind of Amphibians");
-    kind = stdin.readLineSync()!;
+    kind = stdin.readLineSync()!;         // insert value to the variable kind
 
     print("enter the sex");
-    sex = stdin.readLineSync()!;
+    sex = stdin.readLineSync()!;         // insert value to the variable sex
 
     print("enter the long");
-    num? long = num.parse(stdin.readLineSync()!);
+    num? long = num.parse(stdin.readLineSync()!);   // insert value to the variable long
 
     print("the $kind is $sex long's is $long ");
     print("the $kind live in rivers or on lands");
