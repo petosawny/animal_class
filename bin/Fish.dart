7c4b3536325ea626d56a98622486e5127f69b5af
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-14 18:54:18 +0200

    Update Fish.dart

--- bin/Fish.dart
+++ bin/Fish.dart
@@ -1,20 +1,22 @@
-import 'dart:io';
+// import packages and classes
 
+import 'dart:io';
 import 'Animal_class.dart';
 
+// class called Marines extends from the class Animals
 class Marines extends Animals {
+  //variable
   String? food;
 
-
-  void info() {
+  // void Function
+  void generalKnowledge() {
     print("enter the kind of marines");
-    kind = stdin.readLineSync()!;
+    kind = stdin.readLineSync()!;   // insert value to the variable kind
 
     print("enter the food");
-    food = stdin.readLineSync()!;
+    food = stdin.readLineSync()!;  // insert value to the variable food
 
     print("the  $kind eat $food and live in seas");
   }
-  void kindOfFood() {
-  }
+
 }
